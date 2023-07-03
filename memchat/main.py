b17e44b33f064cd5249ca4ecc1f12764556cd454
Serg Sinist <serzh95pro@gmail.com>
2023-07-03 11:55:43 +0400

    modified:   memchat/main.py

--- memchat/main.py
+++ memchat/main.py
@@ -179,8 +179,8 @@ user_data = {}
 
 # Функции
 
-def get_usd_rate():
-    url = 'https://www.cbr.ru/scripts/XML_daily.asp'
+def get_usd_rate(date):
+    url = f'https://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={date.strftime("%d/%m/%Y")}'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'xml')
     valute = soup.find('CharCode', text='USD').find_parent('Valute')
@@ -450,8 +450,36 @@ def calculate_denomination(message, denominations, count, denomination):
 
 @bot.message_handler(func=lambda message: message.text.lower() in USD_RATE_COMMANDS)
 def handle_usd_rate(message):
-    usd_rate = get_usd_rate()
-    text = f'Текущий курс доллара: {usd_rate:.2f} руб.'
+    today = datetime.today()
+    yesterday = today - timedelta(days=1)
+    day_before_yesterday = today - timedelta(days=2)
+
+    usd_rate_today = get_usd_rate(today)
+    usd_rate_yesterday = get_usd_rate(yesterday)
+    usd_rate_day_before_yesterday = get_usd_rate(day_before_yesterday)
+
+    price_diff_today_yesterday = usd_rate_today - usd_rate_yesterday
+    price_diff_yesterday_day_before_yesterday = usd_rate_yesterday - usd_rate_day_before_yesterday
+
+    if price_diff_today_yesterday > 0:
+        arrow_emoji_today_yesterday = '⬆️'
+    elif price_diff_today_yesterday < 0:
+        arrow_emoji_today_yesterday = '⬇️'
+    else:
+        arrow_emoji_today_yesterday = '➡️'
+
+    if price_diff_yesterday_day_before_yesterday > 0:
+        arrow_emoji_yesterday_day_before_yesterday = '⬆️'
+    elif price_diff_yesterday_day_before_yesterday < 0:
+        arrow_emoji_yesterday_day_before_yesterday = '⬇️'
+    else:
+        arrow_emoji_yesterday_day_before_yesterday = '➡️'
+
+    today_str = today.strftime("%d.%m.%Y")
+    yesterday_str = yesterday.strftime("%d.%m.%Y")
+    day_before_yesterday_str = day_before_yesterday.strftime("%d.%m.%Y")
+
+    text = f'💵 Сегодня: {usd_rate_today:.2f}\n💵 {yesterday_str}: {usd_rate_yesterday:.2f} ({arrow_emoji_today_yesterday} {abs(price_diff_today_yesterday):.2f})\n💵 {day_before_yesterday_str}: {usd_rate_day_before_yesterday:.2f} ({arrow_emoji_yesterday_day_before_yesterday} {abs(price_diff_yesterday_day_before_yesterday):.2f})'
     bot.reply_to(message, text)
 
 # ------------------------------------------------------------------------------
