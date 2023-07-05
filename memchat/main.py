7debb048151e2000de92a5d7c7fb36db55ae72f7
Serg Sinist <serzh95pro@gmail.com>
2023-07-05 09:20:59 +0400

    modified:   memchat/main.py

--- memchat/main.py
+++ memchat/main.py
@@ -12,6 +12,7 @@ from datetime import datetime, timedelta
 from sn_cutter import sn_cutter
 from appsaratov_parser import asp_text_message, asp_callback_query
 from usd_rate import handle_usd_rate
+import megacalculator
 
 # Импорты заморских
 import gspread
@@ -439,51 +440,9 @@ def ww_callback_query(call):
 
 ## Конец
 
-import megacalculator
-
 @bot.message_handler(func=lambda message: message.text.lower() in MEGACALC_TRIGGERS)
 def handle_megacalculator(message):
     megacalculator.start_megacalculator(bot, message)
-
-
-## Считывает купюры (Такая красота получилась после рефакторинга)
-# @bot.message_handler(func=lambda message: message.text.lower() in MEGACALC_TRIGGERS)
-# def start_megacalculator(message):
-#     # Define a dictionary of denominations and their corresponding messages
-#     denominations = {
-#         500: "Сколько купюр номиналом 500?",
-#         1000: "Сколько купюр номиналом 1000?",
-#         2000: "Сколько купюр номиналом 2000?",
-#         5000: "Сколько у вас купюр номиналом 5000?"
-#     }
-#     # Start the calculation with the first denomination
-#     count = {}
-#     next_denomination(message, denominations, count)
-
-# def next_denomination(message, denominations, count):
-#     # Get the next denomination to calculate
-#     denomination, question = denominations.popitem()
-#     # Ask the user for the count of bills for the current denomination
-#     bot.send_message(message.chat.id, question)
-#     # Register the next step handler with the current denomination and count
-#     bot.register_next_step_handler(message, calculate_denomination, denominations, count, denomination)
-
-# def calculate_denomination(message, denominations, count, denomination):
-#     try:
-#         # Get the count of bills for the current denomination
-#         count[denomination] = int(message.text)
-#         # If there are more denominations to calculate, move on to the next one
-#         if denominations:
-#             next_denomination(message, denominations, count)
-#         # Otherwise, calculate the total sum and send the result to the user
-#         else:
-#             total_sum = sum(denomination * count[denomination] for denomination in count)
-#             message_text = "Получилось так:\n"
-#             message_text += '\n'.join(f'{denomination} x {count[denomination]}' for denomination in count)
-#             message_text += f'\nИтого: {total_sum}'
-#             bot.send_message(message.chat.id, message_text)
-#     except ValueError:
-#         bot.send_message(message.chat.id, "Пожалуйста, введите число.")
 
 @bot.message_handler(func=lambda message: message.text.lower() in USD_RATE_COMMANDS)
 def start_usd_rate(message):
