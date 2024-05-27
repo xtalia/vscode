49d23ee0cffe96db69e903d8e6991639ebb9e31e
Serg Sinist <serzh95pro@gmail.com>
2024-05-27 12:21:01 +0400

    Update ms_show_all.js

--- memchat/js/ms_show_all.js
+++ memchat/js/ms_show_all.js
@@ -1,7 +1,10 @@
 // ms_show_all.js
 
+console.log('ms_show_all.js loaded');
+
 // Function to show all blocks with class="tab-content"
 function showAllTabContent() {
+    console.log('Running showAllTabContent');
     const tabContents = document.querySelectorAll('.tab-content');
     tabContents.forEach(tabContent => {
         tabContent.style.display = 'block';
