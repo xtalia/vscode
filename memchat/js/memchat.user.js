b9720df8896ddf14639b62bddc9eca0ed9274ab8
Serg Sinist <serzh95pro@gmail.com>
2024-05-27 14:19:02 +0400

    Update memchat.user.js

--- memchat/js/memchat.user.js
+++ memchat/js/memchat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Мемный чат 4
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Набор скриптов
 // @match        https://online.moysklad.ru/*
 // @grant        GM_xmlhttpRequest
@@ -15,7 +15,6 @@
     console.log('Main userscript loaded');
 
     const scriptsToLoad = [
-        'https://raw.githubusercontent.com/xtalia/vscode/main/memchat/js/ms_show_all.js',
         'https://raw.githubusercontent.com/xtalia/vscode/main/memchat/js/price_calculator.js'
     ];
 
@@ -36,6 +35,47 @@
             });
         });
     }
+
+    // Функция для показа всех блоков с class="tab-content"
+    function showAllTabContents() {
+        const hiddenElements = document.querySelectorAll('.tab-content .hidden');
+        hiddenElements.forEach(element => {
+            element.classList.remove('hidden');
+        });
+    }
+
+    // Добавление стилей для контекстного меню
+    GM_addStyle(`
+        .gm-context-menu {
+            z-index: 1000;
+            position: fixed;
+            top: 10px;
+            right: 10px;
+            background-color: #f9f9f9;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 10px;
+            cursor: pointer;
+        }
+    `);
+
+    // Регистрация пункта в контекстном меню
+    GM_registerMenuCommand('Show All Tab Contents', showAllTabContents, 'S');
+
+    // Функция для создания контекстного меню
+    function createContextMenu() {
+        const contextMenu = document.createElement('div');
+        contextMenu.classList.add('gm-context-menu');
+        contextMenu.textContent = 'Show All Tab Contents';
+        contextMenu.addEventListener('click', showAllTabContents);
+        document.body.appendChild(contextMenu);
+    }
+
+    // Запуск функции создания контекстного меню после полной загрузки страницы
+    window.addEventListener('load', () => {
+        setTimeout(createContextMenu, 3000); // Задержка в 3 секунды перед созданием контекстного меню
+    });
 
     // Register menu command to reload scripts
     GM_registerMenuCommand('Обновить скрипты мемные', function() {
