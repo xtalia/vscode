7f01185f6a864738ad840648947176c1c1ce8030
Serg Sinist <serzh95pro@gmail.com>
2024-05-27 12:09:07 +0400

    Update ms_show_all.js

--- memchat/js/ms_show_all.js
+++ memchat/js/ms_show_all.js
@@ -1,15 +1,12 @@
 // ms_show_all.js
-(function() {
-    'use strict';
 
-    // Function to show all blocks with class="tab-content"
-    function showAllTabContent() {
-        const tabContents = document.querySelectorAll('.tab-content');
-        tabContents.forEach(tabContent => {
-            tabContent.style.display = 'block';
-        });
-    }
+// Function to show all blocks with class="tab-content"
+function showAllTabContent() {
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(tabContent => {
+        tabContent.style.display = 'block';
+    });
+}
 
-    // Run the function after the page is fully loaded
-    window.addEventListener('load', showAllTabContent);
-})();
+// Run the function after the page is fully loaded
+window.addEventListener('load', showAllTabContent);
