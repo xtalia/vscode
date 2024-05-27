4d4196f24a574cc331a4c9029b0ded5099572bfc
Serg Sinist <serzh95pro@gmail.com>
2024-05-27 14:05:30 +0400

    Update memchat.user.js

--- memchat/js/memchat.user.js
+++ memchat/js/memchat.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Мемный чат 4
-// @namespace    https://github.com/xtalia/vscode/tree/main/memchat/js
-// @version      1.4
+// @namespace    http://tampermonkey.net/
+// @version      1.5
 // @description  Набор скриптов
 // @match        https://online.moysklad.ru/*
 // @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function() {
@@ -17,19 +18,31 @@
         'https://raw.githubusercontent.com/xtalia/vscode/main/memchat/js/price_calculator.js'
     ];
 
-    scriptsToLoad.forEach(url => {
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: url,
-            onload: function(response) {
-                console.log(`Loaded script from ${url}`);
-                const script = document.createElement('script');
-                script.textContent = response.responseText;
-                document.body.appendChild(script);
-            },
-            onerror: function(error) {
-                console.error(`Failed to load script from ${url}`, error);
-            }
+    function loadScripts() {
+        scriptsToLoad.forEach(url => {
+            GM_xmlhttpRequest({
+                method: 'GET',
+                url: url,
+                onload: function(response) {
+                    console.log(`Loaded script from ${url}`);
+                    const script = document.createElement('script');
+                    script.textContent = response.responseText;
+                    document.body.appendChild(script);
+                },
+                onerror: function(error) {
+                    console.error(`Failed to load script from ${url}`, error);
+                }
+            });
         });
+    }
+
+    // Register menu command to reload scripts
+    GM_registerMenuCommand('Обновить скрипты мемные', function() {
+        console.log('Reloading memchat scripts...');
+        loadScripts();
     });
+
+    // Initial load of scripts
+    loadScripts();
+
 })();
