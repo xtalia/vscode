30ae0d35455e3c0e864b98bcdafa98a077d8fd6b
Serg Sinist <serg.lonetrek@gmail.com>
2024-05-28 10:30:37 +0400

    modified:   .gitignore 	modified:   memchat/data.json 	modified:   memchat/hatikorunner.py 	modified:   memchat/wr_tradein.py

--- memchat/hatikorunner.py
+++ memchat/hatikorunner.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from who_work import get_employee_info
+from wr_tradein import calculate_ideal_price
 from flask_cors import CORS
+from urllib.parse import unquote
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,31 @@ def who_work():
     day = request.args.get('day')
     text = get_employee_info(day)
     return jsonify({'text': text})
+
+@app.route('/tradein', methods=['GET'])
+def tradein():
+    # Извлекаем параметры из запроса и декодируем пробелы
+    model = unquote(request.args.get('model', ''))
+    memory = unquote(request.args.get('memory', ''))
+    battery_capacity = int(request.args.get('battery_capacity', 0))
+    package = unquote(request.args.get('package', ''))
+    back_cover = request.args.get('back_cover', type=bool, default=False)
+    screen = request.args.get('screen', type=bool, default=False)
+    condition = unquote(request.args.get('condition', ''))
+
+    # Вызываем функцию для расчета идеальной цены
+    ideal_price = calculate_ideal_price(
+        model,
+        memory,
+        battery_capacity,
+        package,
+        back_cover,
+        screen,
+        condition
+    )
+
+    # Возвращаем JSON-ответ с идеальной ценой
+    return jsonify({'ideal_price': ideal_price})
     
 if __name__ == '__main__':
     app.run(debug=True)
