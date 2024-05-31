08b05df3f56d1523073469f7e15baffc95ccfd10
Serg Sinist <serg.lonetrek@gmail.com>
2024-05-31 19:23:57 +0400

    modified:   memchat/hatikorunner.py 	new file:   memchat/wsgi.py

--- memchat/hatikorunner.py
+++ memchat/hatikorunner.py
@@ -65,4 +65,5 @@ def memchat():
 
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=False)
+    # app.run(port=5001,debug=False)
+    pass
---
+++ memchat/wsgi.py
@@ -0,0 +1,4 @@
+from hatikorunner import app  # замените на имя вашего файла приложения Flask
+
+if __name__ == "__main__":
+    app.run()
